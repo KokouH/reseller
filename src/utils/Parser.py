7b2e0d0b20b84c0c77f4af08f9d1c4fe6d2cfa7f
KokocuK <randomtv855@gmail.com>
2025-02-06 08:18:00 +0300

    Поправил парсер, не использовалась мидимальная задержка Правильно реализовал сессии

--- src/utils/Parser.py
+++ src/utils/Parser.py
@@ -8,84 +8,94 @@ import json
 import os
 
 class Parser:
-    def __init__(self, with_retry: bool = True) -> None:
-        self.last_page: None | str = None
-        self.ses: requests.Session = requests.Session()
-        self.with_retry = with_retry
-        self.retry_time = 90
-        self.retry_count = 3
-        self.min_delay = 3 # in sec
-        self.last_request_time = time.time()
+	def __init__(self, with_retry: bool = True) -> None:
+		self.last_page: None | str = None
+		self.ses: requests.Session = requests.Session()
+		self.sessions: List[requests.Session] | None = None
+		self.with_retry = with_retry
+		self.retry_time = 90
+		self.retry_count = 3
+		self.min_delay = 3 # in sec
+		self.last_request_time = time.time()
 
-        self._proxies = []
-        self._pr_index = 0
-        self._init_proxy()
+		self._proxies = []
+		self._pr_index = 0
+		# self._init_proxy()
 
-    def _init_proxy(self):
-        with open('proxies.txt', 'r') as file:
-            l = file.read().split('\n')
-            l = [i for i in l if i]
+	def _init_proxy(self):
+		with open('proxies.txt', 'r') as file:
+			l = file.read().split('\n')
+			l = [i for i in l if i]
 
-        if l:
-            logger.info(f"Found {len(l)} proxies")
-            self._proxies = [{"http":"http://"+proxy_s, "https":"http://"+proxy_s} for proxy_s in l]
+		if l:
+			logger.info(f"Found {len(l)} proxies")
+			self._proxies = [{"http":"http://"+proxy_s, "https":"http://"+proxy_s} for proxy_s in l]
+			for proxy in self._proxies:
+				ses = requests.Session()
+				ses.proxies.update(proxy)
 
-    def ses_get(self, *args, **kwargs) -> requests.Response:
-        cout = 0
-        while cout < self.retry_count:
-            if time.time() - self.last_request_time < 2:
-                time.sleep(2 - time.time() + self.last_request_time)
-            if not self._proxies:
-                res = self.ses.get(*args, **kwargs)
-            else:
-                proxy = self._proxies[self._pr_index]
-                res = self.ses.get(*args, **kwargs, proxies=proxy)
-                self._pr_index += 1
-                if self._pr_index >= len(self._proxies):
-                    self._pr_index = 0
-            self.last_request_time = time.time()
-            if res.status_code == 200:
-                return res
-            for _ in range(self.retry_time):
-                time.sleep(1)
-            cout += 1
+	def ses_get(self, *args, **kwargs) -> requests.Response:
+		cout = 0
+		while cout < self.retry_count:
+			if time.time() - self.last_request_time < self.min_delay:
+				time.sleep(self.min_delay - time.time() + self.last_request_time)
+			if not self._proxies:
+				res = self.ses.get(*args, **kwargs)
+			else:
+				ses = self.sessions[self._pr_index]
+				res = ses.get(*args, **kwargs)
+				self._pr_index += 1
+				if self._pr_index >= len(self._proxies):
+					self._pr_index = 0
+			self.last_request_time = time.time()
+			if res.status_code == 200:
+				return res
+			logger.info(f"lag parser 90 sec")
+			for _ in range(self.retry_time):
+				time.sleep(1)
+			cout += 1
 
-        return None
+		return None
 
-    def get_itemid_from_page(self, page: str) -> int | None:
-        if page == None:
-            return None
-        x = page.find('Market_LoadOrderSpread(') + 24
-        itemid = None
-        try:
-            itemid = int(page[x: x  + page[x:].find(' ')])
-        except:
-            pass 
-        finally:
-            return itemid
-        
-    def get_item_page(self, hash_name: str, appid: int | str = 252490) -> str | None:
-        res = self.ses_get(f'https://steamcommunity.com/market/listings/{appid}/{hash_name}')
-        if res.status_code != 200:
-            self.last_page = res.text
-            return None
-        self.last_page = res.text
-        return res.text
-    
-    def get_item_histogram(self, itemid: int) -> Any | None:
-        res = self.ses_get(f'https://steamcommunity.com/market/itemordershistogram?country=US&language=english&currency=1&item_nameid={itemid}')
-        
-        if res:
-            return res.json()
-        return None
-    
-    def get_history(self, page: str) -> List:
-        start = page.find('var line1=')
-        raw_history = json.loads(page[start + 10 : start + page[start:].find(']]') + 2]) # 10 - 'var line1=' size
+	def get_itemid_from_page(self, page: str) -> int | None:
+		if page == None:
+			return None
+		x = page.find('Market_LoadOrderSpread(') + 24
+		itemid = None
+		try:
+			itemid = int(page[x: x  + page[x:].find(' ')])
+		except:
+			pass 
+		finally:
+			return itemid
+		
+	def get_item_page(self, hash_name: str, appid: int | str = 252490) -> str | None:
+		headers = {
+			'referer': 'https://steamcommunity.com/market/',
+			'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36',
+			
+		}
+		res = self.ses_get(f'https://steamcommunity.com/market/listings/{appid}/{hash_name}', headers=headers)
+		if res.status_code != 200:
+			self.last_page = res.text
+			return None
+		self.last_page = res.text
+		return res.text
+	
+	def get_item_histogram(self, itemid: int) -> Any | None:
+		res = self.ses_get(f'https://steamcommunity.com/market/itemordershistogram?country=US&language=english&currency=1&item_nameid={itemid}')
+		
+		if res:
+			return res.json()
+		return None
+	
+	def get_history(self, page: str) -> List:
+		start = page.find('var line1=')
+		raw_history = json.loads(page[start + 10 : start + page[start:].find(']]') + 2]) # 10 - 'var line1=' size
 
-        for i, el in enumerate(raw_history):
-            raw_history[i][0] = datetime.datetime.strptime(el[0][:14], '%b %d %Y %H') # Feb 18 2016 01: +0
+		for i, el in enumerate(raw_history):
+			raw_history[i][0] = datetime.datetime.strptime(el[0][:14], '%b %d %Y %H') # Feb 18 2016 01: +0
 
-        history = raw_history
+		history = raw_history
 
-        return (history)
+		return (history)
